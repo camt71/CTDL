b5c5aa50c7d6ab460032dec618e35b80b70189ca
camt71 <124553258+camt71@users.noreply.github.com>
2023-09-30 21:47:36 +0700

    Update MyCaesar.java

--- MyCaesar.java
+++ MyCaesar.java
@@ -12,12 +12,18 @@ public class MyCaesar {
 
 //array
 	public char encryptChar(char c) {
-		if (c >= 65 && c <= 90) {
-			return ALPHABET[(c + n) % 26];
-		} else {
-			return (char) ('0' + c);
-
+		int res = -1;
+		for (int i = 0; i < ALPHABET.length; i++) {
+			if (ALPHABET[i] == c) {
+				res = i;
+			}
 		}
+
+		if (res + n > 25) {
+			return ALPHABET[n - (24 - res) - 1];
+		} else
+			return ALPHABET[(res + n) % 26];
+
 	}
 
 	public String encrypt(String input) {
@@ -29,14 +35,20 @@ public class MyCaesar {
 		return res;
 	}
 
+	
 	public char decryptChar(char c) {
-		if (c >= 65 && c <= 90) {
-			return ALPHABET[(c - n) % 26];
-		} else {
-			return (char) ('0' - c);
-
+		int res = -1;
+		for (int i = 0; i < ALPHABET.length; i++) {
+			if (ALPHABET[i] == c) {
+				res = i;
+			}
 		}
+		if (res - n < 0) {
+			return ALPHABET[(24 - n) + res + 1];
+		} else
+			return ALPHABET[(res - n) % 26];
 	}
+
 
 	public String decrypt(String input) {
 		String res = "";
